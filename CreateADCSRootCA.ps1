df3758272fbb7b9e2ca57ef71c0ef759e0dd68dd
lahavana <36070872+lahavana@users.noreply.github.com>
2025-09-02 08:56:04 +0200

    Create CreateADCSRootCA.ps1

---
+++ CreateADCSRootCA.ps1
@@ -0,0 +1,65 @@
+﻿configuration CreateADCSRootCA
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [PSCredential]$adminCreds
+    )
+
+    Import-DscResource -ModuleName xAdcsDeployment, xDSCDomainjoin, ComputerManagementDSC
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+
+        # Domain Join
+        xDSCDomainjoin JoinDomain
+        {
+            DomainName = $adminCreds.UserName.Split('\')[0]  # Extract domain from "domain\username"
+            Credential = $adminCreds
+        }
+
+        # Reboot after domain join
+        PendingReboot RebootAfterDomainJoin
+        {
+            Name = "RebootAfterDomainJoin"
+            DependsOn = "[xDSCDomainjoin]JoinDomain"
+        }
+
+        # Install ADCS Role
+        WindowsFeature ADCSInstall
+        {
+            Ensure = "Present"
+            Name = "ADCS-Cert-Authority"
+            DependsOn = "[PendingReboot]RebootAfterDomainJoin"
+        }
+
+        # Install Management Tools
+        WindowsFeature ADCSMgmt
+        {
+            Ensure = "Present"
+            Name = "RSAT-ADCS"
+            DependsOn = "[WindowsFeature]ADCSInstall"
+        }
+
+        # Configure Enterprise Root CA
+        xADCSCertificationAuthority ADCSRootCA
+        {
+            Ensure = "Present"
+            Credential = $adminCreds
+            CAType = "EnterpriseRootCA"
+            DependsOn = "[WindowsFeature]ADCSMgmt"
+        }
+
+        # Optional: Reboot after CA installation
+        PendingReboot RebootAfterCAInstall
+        {
+            Name = "RebootAfterCAInstall"
+            DependsOn = "[xADCSCertificationAuthority]ADCSRootCA"
+        }
+    }
+}
